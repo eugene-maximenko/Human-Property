a136f83f4062ffe38eb96833373cdfc9083ea717
tiger3120 <zhek4770@gmail.com>
2021-09-25 00:02:16 +0300

    Added function signatures

---
+++ controllers/user.controller.js
@@ -0,0 +1,21 @@
+class UserController {
+    async createUser(req, res) {
+
+    }
+
+    async getUsers(req, res) {
+
+    }
+
+    async getOneUser(req, res) {
+
+    }
+
+    async updateUser(req, res) {
+
+    }
+
+    async deleteUser(req, res) {
+
+    }
+}
